0b3ba0920777eb6c509c566603cfe2f9668734a9
ehko <ehko@debianLenovo>
2018-04-17 10:51:14 +0200

    Modificaciones con Update User

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -138,7 +138,7 @@ function getUser(req,res){
 /**
  * Paginación, obtiene el número de página
  * @param {*} req 
- * @param {*} resp 
+ * @param {*} res
  */
 function getUsers(req, res){
     var identity_user_id = req.user.sub;
@@ -170,6 +170,34 @@ function getUsers(req, res){
     }); 
 
 }
+/**
+ * Edición de datos de usuario
+ */
+function updateUser(req, res){
+    var userId = req.params.id;
+
+    // se le pasa a mongoose para actualizar los datos
+    var update = req.body;
+
+
+    //borrar propiedad password
+    delete update.password;
+
+
+    if(userId != req.user.sub){
+        return  res.status(500).send({message:'No tienes permiso para actualizar los datos del usuario'});
+    }
+
+    User.findByIdAndUpdate(userId, update, {new:true}, (err,userUpdated) => {
+        if(err) return res.status(500).send({message:'Error en la petición'}) ;
+        
+        if(!userUpdated) return res.status(404).send({message:'No se ha podido actualizar el usuario'});
+
+        return res.status(200).send({user:userUpdated});
+
+    });
+
+}
 
 
 module.exports = {
@@ -178,6 +206,7 @@ module.exports = {
     saveUser,
     loginUser,
     getUser,
-    getUsers
+    getUsers,
+    updateUser
 }
 
